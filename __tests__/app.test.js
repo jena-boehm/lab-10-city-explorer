9d8432b48facbe8a8c74527148d51bbc3532eb2f
Jena Boehm <boehmjena@gmail.com>
2020-11-10 10:30:01 -0800

    working weather test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const { mungeLocation, mungeWeather } = require('../utils.js');
+const weatherData = require('../data/weather.json');
 
 describe('app routes', () => {
   describe('routes', () => {
@@ -42,33 +43,34 @@ describe('app routes', () => {
 
     test('returns weather', () => {
 
-      const location = [{
-        'place_id': '282983083',
-        'licence': 'https://locationiq.com/attribution',
-        'osm_type': 'relation',
-        'osm_id': '186579',
-        'boundingbox': [
-          '45.432536',
-          '45.6528812',
-          '-122.8367489',
-          '-122.4720252'
-        ],
-        'lat': '45.5202471',
-        'lon': '-122.6741949',
-        'display_name': 'Portland, Multnomah County, Oregon, USA',
-        'class': 'place',
-        'type': 'city',
-        'importance': 0.75356571743377,
-        'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
-      }];
-
       const expectation =
-      {
-        forecast: 'Light shower rain',
-        time: '11/9/2020'
-      };
+      [
+        {
+          'forecast': 'Scattered clouds', 'time': '2020-05-05'
+        },
+        {
+          'forecast': 'Light snow', 'time': '2020-05-06'
+        }, 
+        {
+          'forecast': 'Few clouds', 'time': '2020-05-07'
+        }, 
+        {
+          'forecast': 'Few clouds', 'time': '2020-05-08'
+        }, 
+        {
+          'forecast': 'Broken clouds', 'time': '2020-05-09'
+        }, 
+        {
+          'forecast': 'Overcast clouds', 'time': '2020-05-10'
+        }, 
+        {
+          'forecast': 'Overcast clouds', 'time': '2020-05-11'
+        }, 
+        {
+          'forecast': 'Light rain', 'time': '2020-05-12'
+        }];
 
-      const result = mungeWeather(location);
+      const result = mungeWeather(weatherData);
       expect(result).toEqual(expectation);
     });
   });
